8a93708d58f3bf6e21a3d997f9a20593cafd11e8
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-08-15 13:39:51 -0700

    Moving all tests to one file

--- frontend/test/ui/test_homepage.py
+++ frontend/test/ui/test_homepage.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import subprocess
 
 import pytest
 import requests
 
 from pages.desktop.home import Home
+from pages.desktop.detail import Detail
 from pages.desktop.privacy import Privacy
 from pages.desktop.terms import Terms
 from pages.desktop.about import About
@@ -64,3 +66,38 @@ def test_incomplete_email(base_url, selenium):
     assert page.signup_footer.is_stay_informed_displayed
     page.signup_footer.sign_up('test')
     assert page.signup_footer.is_stay_informed_displayed
+
+
+ff = subprocess.Popen("firefox --verison", shell=True, stdout=subprocess.PIPE,
+                      stderr=subprocess.STDOUT)
+
+
+@pytest.mark.nondestructive
+@pytest.mark.skipif('57' not in ff.stdout.readlines(),
+                    reason="57 needed to install unsigned addon")
+def test_install_of_test_pilot_addon(
+        base_url, selenium, firefox, notifications):
+    """Check that the testpilot addon is installable and installs."""
+    page = Home(selenium, base_url).open()
+    experiments = page.header.click_install_button()
+    firefox.browser.wait_for_notification(notifications.AddOnInstallComplete)
+    assert 'Welcome to Test Pilot!' in experiments.welcome_popup.title
+
+
+@pytest.mark.nondestructive
+@pytest.mark.skipif('57' not in ff.stdout.readlines(),
+                    reason="57 needed to install unsigned addon")
+def test_enable_experiment(base_url, selenium, firefox, notifications):
+    """Test enabling of an experiment."""
+    page = Home(selenium, base_url).open()
+    experiments = page.header.click_install_button()
+    experiments.welcome_popup.close()
+    experiment = experiments.find_experiment(experiment='Dev Example')
+    experiment.enable()
+    firefox.browser.wait_for_notification(
+        notifications.AddOnInstallComplete).close()
+    firefox.browser.wait_for_notification(
+        notifications.AddOnInstallConfirmation).install()
+    firefox.browser.wait_for_notification(
+        notifications.AddOnInstallComplete).close()
+    assert Detail(selenium, base_url).enabled_popup.is_popup_displayed()
